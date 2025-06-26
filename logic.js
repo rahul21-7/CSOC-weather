3832a830da2f99978f20c3c52c580846d8511368
rahul21-7 <dasari2007rahul2007@gmail.com>
2025-06-27 00:44:09 +0530

    You have to use your own url with your api key, sorry

---
+++ logic.js
@@ -0,0 +1,31 @@
+const city = document.querySelector('#city');
+const weather = document.querySelector('#description');
+const temperature = document.querySelector('#temperature');
+const humidity = document.querySelector('#humidity');
+const wind = document.querySelector('#wind');
+// const description = document.querySelector('#description');
+const button = document.querySelector('#getWeather');
+
+// const URL = { please use a valid URL here, Ive removed mine for privacy reasons };
+
+button.addEventListener('click', () => {
+    getWeather();
+});
+
+const getWeather = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    const countryName = data.name;
+    city.textContent += " : " + countryName;
+    const hum = data.main.humidity;
+    humidity.textContent += " : " + hum;
+    const temperatureVal = data.main.temp;
+    temperature.textContent += " : " + temperatureVal;
+    const winds = data.wind.speed;
+    wind.textContent += " : " + winds;
+    const weatherVal = data.weather[0].description;
+    weather.textContent += " : " + weatherVal;
+}
+
+// getWeather();
+
